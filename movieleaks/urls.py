9a46c561f18532a80adf2a6c4130e84a4d14abd6
Dennis Juma <jumaspay@gmail.com>
2018-02-20 03:04:45 +0300

    pwares linked

--- movieleaks/urls.py
+++ movieleaks/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
 urlpatterns = [
     url(r'^$', views.index, name='home'),
 
